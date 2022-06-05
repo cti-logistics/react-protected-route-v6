f041415396ab110765f423482d5a53e1080d60ad
suthicha <suthicha@outlook.com>
2022-06-06 00:16:43 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import { useRoutes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import routes from './routes';
-const auth = {
-  userName: null,
-  token: null,
-};
 
 const App = () => {
   const userAuth = useSelector((state) => state.auth);
